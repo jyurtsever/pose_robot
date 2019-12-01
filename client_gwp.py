2f2dfb504b05b7f30b2c07ebbdeaa856c417e0fb
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-11-30 21:09:09 -0800

    changed hostname in client to jyurtseverlatte....

--- client_gwp.py
+++ client_gwp.py
@@ -4,9 +4,13 @@ import socket
 import sys
 import pickle
 import struct ### new code
+HOST = 'jyurtsever@latte.csua.berkeley.edu'
+PORT = 8089
+
+
 cap=cv2.VideoCapture(0)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
+clientsocket.connect((HOST, PORT))
 while True:
     ret,frame=cap.read()
     data = pickle.dumps(frame) ### new code
