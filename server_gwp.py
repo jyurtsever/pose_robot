3dad9ca992fb8ad84d1faeab175c3d5429fdc281
Joshua Yurtsever <joshua.yurtsver@berkeley.edu>
2019-11-30 21:25:11 -0800

    server changes

--- server_gwp.py
+++ server_gwp.py
@@ -20,13 +20,13 @@ conn,addr=s.accept()
 
 ### new
 data = ""
-payload_size = struct.calcsize("H")
+payload_size = struct.calcsize("L")
 while True:
     while len(data) < payload_size:
         data += conn.recv(4096)
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
-    msg_size = struct.unpack("H", packed_msg_size)[0]
+    msg_size = struct.unpack("L", packed_msg_size)[0]
     while len(data) < msg_size:
         data += conn.recv(4096)
     frame_data = data[:msg_size]
