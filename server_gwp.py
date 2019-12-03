5a5a3601cf86f8fc986fcf14040550b757dd5b8f
Joshua Yurtsever <joshua.yurtsever@berkeley.edu>
2019-12-03 12:17:05 -0800

    reverted back to coco

--- server_gwp.py
+++ server_gwp.py
@@ -18,42 +18,7 @@ body_parts = ['Nose', 'Neck', 'Right Shoulder', 'Right Elbow', 'Right Wrist',
               'Left Shoulder', 'Left Elbow', 'Left Wrist', 'Right Hip', 'Right Knee', 'Right Ankle',
               'Left Hip', 'Left Knee', 'LAnkle', 'Right Eye', 'Left Eye', 'Right Ear', 'Left Ear', 'Background']
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# print('Socket created')
-#
-# s.bind((HOST, PORT))
-# print('Socket bind complete')
-# s.listen(10)
-# print('Socket now listening')
-#
-# conn, addr = s.accept()
-#
-# data = b'' ### CHANGED
-# payload_size = struct.calcsize("L") ### CHANGED
-#
-# while True:
-#
-#     # Retrieve message size
-#     while len(data) < payload_size:
-#         data += conn.recv(4096)
-#
-#     packed_msg_size = data[:payload_size]
-#     data = data[payload_size:]
-#     msg_size = struct.unpack("L", packed_msg_size)[0] ### CHANGED
-#
-#     # Retrieve all data based on message size
-#     while len(data) < msg_size:
-#         data += conn.recv(4096)
-#
-#     frame_data = data[:msg_size]
-#     data = data[msg_size:]
-#
-#     # Extract frame
-#     frame = pickle.loads(frame_data)
-#     print(frame.shape)
-#     # Display
-#     # cv2.imshow('frame', frame)
-#     # cv2.waitKey(1)
+
 def forward(frame):
     inWidth = frame.shape[1]
     inHeight = frame.shape[0]
@@ -79,7 +44,7 @@ def get_points(out, frame):
     H = out.shape[2]
     W = out.shape[3]
     # Empty list to store the detected keypoints
-    threshold = .15
+    threshold = .20
     points = []
     for i in range(len(body_parts)):
         # confidence map of corresponding body's part.
@@ -141,7 +106,7 @@ if __name__ == '__main__':
 
     # Read the network into Memory
     print("Initializing Model")
-    caffe.set_device(0)
+    caffe.set_device(1)
     caffe.set_mode_gpu()
     # net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
     net = caffe.Net(protoFile, weightsFile, caffe.TEST)
